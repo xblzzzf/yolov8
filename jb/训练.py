46e5a60341eb3c86093711271405388cb76cc2b8
xblzzzf <iza1382790zhilun1@163.com>
2026-04-17 10:15:30 +0800

    update jb files

--- jb/训练.py
+++ jb/训练.py
@@ -1,13 +1,20 @@
+import os
 from ultralytics import YOLO
-# Load a COCO-pretrained YOLOv8n model
-# 加载预训练的YOLOv8n模型
-model = YOLO("yolov8s.pt")
 
-# Display model information (optional)
-model.info()
+# 解决 OpenMP 冲突
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
-# Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+if __name__ == '__main__':
+    # Load a COCO-pretrained YOLOv8s model
+    # 加载预训练的YOLOv8s模型
+    model = YOLO("yolov8s.pt")
 
-# Run inference with the YOLOv8n model on the 'bus.jpg' image
-results = model("path/to/bus.jpg")
+    # Display model information (optional)
+    model.info()
+
+    # Train the model on the COCO8 example dataset for 100 epochs
+    results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+
+    # Run inference with the YOLOv8s model on a test image
+    # 使用内置的测试图像
+    results = model("ultralytics/assets/bus.jpg")
